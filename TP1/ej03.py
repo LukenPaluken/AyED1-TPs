3134db6b5b003e0137e5264fa62ec5c7b4dce8c0
Lucas Paluci <lucas360usa@gmail.com>
2024-08-17 22:44:40 -0300

    Added documentation - Actually finished

--- TP1/ej03.py
+++ TP1/ej03.py
@@ -1,4 +1,14 @@
 def calcular_gastos(viajes, valor_pasaje):
+    """
+    Calcula el total gastado en pasajes de subte en un mes, aplicando descuentos según la cantidad de viajes realizados.
+
+    Argumentos:
+        viajes (int): La cantidad de viajes realizados en el mes.
+        valor_pasaje (float): El valor actual de un boleto.
+
+    Retorna:
+        float: El total gastado considerando los descuentos aplicables.
+    """
     total_gastado = valor_pasaje * viajes
     if 1 <= viajes <= 20:
         return total_gastado
@@ -11,6 +21,12 @@ def calcular_gastos(viajes, valor_pasaje):
 
 
 def app():
+    """
+    Función principal de la aplicación que interactúa con el usuario para calcular el total gastado en pasajes de subte.
+
+    Solicita al usuario la cantidad de viajes realizados y el valor de un boleto,
+    luego utiliza la función `calcular_gastos` para determinar el costo total con descuentos aplicables.
+    """
     while True:
         try:
             cantidad = int(input("Ingrese la cantidad de viajes realizados: "))
